9caa6e0bfa2f797888ad5f3b7a6a60436a3c4af9
Kelvin <you@example.com>
2026-07-21 14:32:17 +0800

    Test CI/CD pipeline deploy

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from my Dockerized Flask app, Kelvin!"
+    return "Hello from my Dockerized Flask app, Kelvin! Deployed via GitHub Actions 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
