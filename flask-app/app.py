e4c378df6e542ebb478a12743bbf4e43c2736803
Kelvin <you@example.com>
2026-07-22 11:28:07 +0800

    Trigger redeploy on hardened instance

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from my Dockerized Flask app, Kelvin! Deployed via GitHub Actions 🚀"
+    return "Hello from my Dockerized Flask app, Kelvin! Now IMDSv2-enforced and encrypted 🔒"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
