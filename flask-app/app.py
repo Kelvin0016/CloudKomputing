c593566a566de9379cd7087532c9049f4be8d38a
Kelvin <you@example.com>
2026-08-08 11:06:14 +0800

    Trigger redeploy to new EC2 instance take 2.

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from my Dockerized Flask app, Kelvin! Redeploy for Kubernetes."
+    return "Hello from my Dockerized Flask app, Kelvin! Redeploy for Kubernetes. Take 2."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
